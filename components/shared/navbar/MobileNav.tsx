34e002dad2be0e97ed1a5d299d640b87331926be
Ishan Veer <ishanpveer@gmail.com>
2024-07-10 12:41:24 +0530

    added global search ui

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import {
   Sheet,
@@ -9,9 +10,41 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { SignedOut } from "@clerk/nextjs";
+import { sidebarLinks } from "@/constants/constants";
+import { usePathname } from "next/navigation";
 
 const NavContent = () => {
-  return <div>navcontent</div>;
+  const pathname = usePathname();
+
+  return (
+    <section className="flex h-full flex-col gap-6 pt-16">
+      {sidebarLinks.map((link) => {
+        const isActive =
+          (pathname.includes(link.route) && link.route.length > 1) ||
+          pathname === link.route;
+
+        return (
+          <SheetClose asChild key={link.route}>
+            <Link
+              href={link.route}
+              className={`${isActive ? "primary-gradient rounded-lg text-light-900" : "text-dark300_light900"} flex items-center justify-start gap-4 bg-transparent p-4`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={23}
+                height={23}
+                className={`${isActive ? "" : "invert-colors"}`}
+              />
+              <p className={`${isActive ? "base-bold" : "base-medium"}`}>
+                {link.label}
+              </p>
+            </Link>
+          </SheetClose>
+        );
+      })}
+    </section>
+  );
 };
 
 const MobileNav = () => {
@@ -48,7 +81,14 @@ const MobileNav = () => {
             <SheetClose asChild>
               <Link href="/sign-in">
                 <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
-                  <span className="primary-text-gradient">Sign in</span>
+                  <span className="primary-text-gradient">Login</span>
+                </Button>
+              </Link>
+            </SheetClose>
+            <SheetClose asChild>
+              <Link href="/sign-in">
+                <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                  Sign Up
                 </Button>
               </Link>
             </SheetClose>
