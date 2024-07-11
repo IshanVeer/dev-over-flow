677a6af68c9b29e61fa1177771e81c02f17e597a
Ishan Veer <ishanpveer@gmail.com>
2024-07-11 20:32:49 +0530

    created local search

--- app/(root)/(Home)/page.tsx
+++ app/(root)/(Home)/page.tsx
@@ -1,3 +1,25 @@
+import LocalSearch from "@/components/shared/LocalSearch";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
-  return <div className="h1-bold"></div>;
+  return (
+    <>
+      {/* Heading and ask a question button */}
+      <div className="flex items-center justify-between">
+        <h1 className="h1-bold text-dark400_light900">All Questions</h1>
+        <Link href="/" className="cursor-pointer">
+          <Button className="primary-gradient hover:primary-hover-gradient min-h-[46px] w-full px-4 py-3 text-light-850 transition duration-150">
+            Ask a Question
+          </Button>
+        </Link>
+      </div>
+      {/* Search and filter */}
+      <div className="mt-8">
+        <LocalSearch placeholder="Search for Questions Here..." />
+      </div>
+      {/* questions */}
+      <div></div>
+    </>
+  );
 }
--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -17,7 +17,7 @@ const GlobalSearch = () => {
           type="text"
           placeholder="Search Globally"
           value=""
-          className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
+          className="paragraph-regular no-focus placeholder:paragraph-regular placeholder background-light800_darkgradient border-none shadow-none outline-none"
         />
       </div>
     </div>
