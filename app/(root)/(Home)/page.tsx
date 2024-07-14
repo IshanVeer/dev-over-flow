fba7b237c02524e2b75957eebdcb603c030dac2a
Ishan Veer <ishanpveer@gmail.com>
2024-07-14 14:58:34 +0530

    added ask a questions page

--- app/(root)/(Home)/page.tsx
+++ app/(root)/(Home)/page.tsx
@@ -48,7 +48,7 @@ export default function Home() {
       {/* Heading and ask a question button */}
       <div className="flex items-center justify-between">
         <h1 className="h1-bold text-dark400_light900">All Questions</h1>
-        <Link href="/" className="cursor-pointer">
+        <Link href="/ask-question" className="cursor-pointer">
           <Button className="primary-gradient hover:primary-hover-gradient min-h-[46px] w-full px-4 py-3 text-light-850 transition duration-150">
             Ask a Question
           </Button>
