fa73359edf4b64f46bb758c738e1d5ffe00227f1
Ishan Veer <ishanveer@Ishans-MacBook-Air.local>
2024-08-12 12:09:07 +0530

    fixed tags validations and made design changess to the button

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,12 @@
 import QuestionForm from "@/components/forms/QuestionForm";
+import { getUserById } from "@/lib/actions/users.action";
+import { auth } from "@clerk/nextjs/server";
 import React from "react";
 
 function AskQuestion() {
+  const { userId } = auth();
+  const mongoUser = getUserById({ userId });
+  console.log(mongoUser, "mongoUser");
   return (
     <div>
       <h1 className="h1-bold text-dark400_light900 mb-8 ">
