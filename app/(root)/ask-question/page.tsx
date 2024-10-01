a4a00281b1b128f886fa591189fb186d5c5e5c41
Ishan Veer <ishanpveer@gmail.com>
2024-10-01 08:54:37 +0530

    creating questions collection in mongoDB

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,18 +1,23 @@
 import QuestionForm from "@/components/forms/QuestionForm";
 import { getUserById } from "@/lib/actions/users.action";
 import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import React from "react";
 
-function AskQuestion() {
+async function AskQuestion() {
   const { userId } = auth();
-  const mongoUser = getUserById({ userId });
+  if (!userId) {
+    redirect("/sign-in");
+  }
+  const mongoUser = await getUserById({ userId });
+  console.log("user");
   console.log(mongoUser, "mongoUser");
   return (
     <div>
       <h1 className="h1-bold text-dark400_light900 mb-8 ">
         Ask a public question
       </h1>
-      <QuestionForm />
+      <QuestionForm mongoUserId={JSON.stringify(mongoUser._id)} />
     </div>
   );
 }
