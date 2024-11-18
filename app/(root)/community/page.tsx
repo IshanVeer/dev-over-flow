06e3390f65fe26d895fe9ff594d2b4d34049f977
Ishan Veer <ishanveer@Ishans-MacBook-Air.local>
2024-11-18 18:43:19 +0530

    Added local search to community page

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -1,7 +1,20 @@
+import RenderFilters from "@/components/shared/RenderFilters";
+import LocalSearch from "@/components/shared/search/LocalSearch";
+import { UserFilters } from "@/constants/filters";
 import React from "react";
 
 const Community = () => {
-  return <div>community</div>;
+  return (
+    <>
+      <h1 className="h1-bold text-dark400_light900">All Users</h1>
+      <div className="mt-8 flex items-center gap-8 max-sm:flex-col">
+        <LocalSearch placeholder="Search amazing minds here..." />
+        <div className="max-sm:w-full ">
+          <RenderFilters filters={UserFilters} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Community;
