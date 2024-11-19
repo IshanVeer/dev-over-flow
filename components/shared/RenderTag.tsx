b7f5a24e611d5df53d1a7386cef4a3b805b2a25f
Ishan Veer <ishanpveer@gmail.com>
2024-11-19 13:51:25 +0530

    added community page

--- components/shared/RenderTag.tsx
+++ components/shared/RenderTag.tsx
@@ -3,7 +3,7 @@ import { Badge } from "@/components/ui/badge";
 import Link from "next/link";
 
 interface Props {
-  _id: string;
+  _id?: string;
   name: string;
   totalQuestions?: number;
   showCount?: boolean;
@@ -12,7 +12,7 @@ interface Props {
 const RenderTag = ({ _id, name, totalQuestions, showCount }: Props) => {
   return (
     <Link href={name} className="flex items-center justify-between">
-      <Badge className="background-light800_dark400 subtle-medium  text-light400_light500 rounded-md px-4 py-2 uppercase">
+      <Badge className="background-light800_dark400 subtle-medium text-light400_light500   rounded-md px-4 py-2 uppercase">
         {name}
       </Badge>
       {showCount && (
