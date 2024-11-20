3740f0bb52197acade3845b8284ce1f25130ab81
Ishan Veer <ishanpveer@gmail.com>
2024-11-20 15:17:30 +0530

    created tags page

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,6 +1,7 @@
 import NoResult from "@/components/shared/NoResult";
 import RenderFilters from "@/components/shared/RenderFilters";
 import LocalSearch from "@/components/shared/search/LocalSearch";
+import { Badge } from "@/components/ui/badge";
 import { TagFilters } from "@/constants/filters";
 import { getAllTags } from "@/lib/actions/tags.action";
 
@@ -20,9 +21,27 @@ const Tags = async () => {
       </div>
       <section className="my-16 ">
         {result.tags.length > 0 ? (
-          <div className="grid grid-cols-3 gap-2">
+          <div className="grid grid-cols-3 gap-4">
             {result.tags.map((tag) => (
-              <div key={tag.name}>{tag.name}</div>
+              <div
+                className="background-light900_dark200 light-border shadow-light100_dark100 rounded-lg border  px-6 py-8"
+                key={tag._id}
+              >
+                <Badge className="background-light800_dark400 paragraph-semibold text-dark500_light700 rounded-md px-4 py-2">
+                  {tag.name}
+                </Badge>
+                <p className="small-regular text-dark500_light700 my-5">
+                  JavaScript, often abbreviated as JS, is a programming language
+                  that is one of the core technologies of the World Wide Web,
+                  alongside HTML and CSS
+                </p>
+                <p className="small-semibold text-dark400_light500 ">
+                  <span className="body-semibold text-primary-gradient">
+                    {tag.questions.length}+
+                  </span>{" "}
+                  Questions{" "}
+                </p>
+              </div>
             ))}
           </div>
         ) : (
