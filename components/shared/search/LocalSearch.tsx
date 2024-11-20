4b50188ba3079efb5457354a642ad7a5836f6497
Ishan Veer <ishanveer@Ishans-MacBook-Air.local>
2024-11-20 10:12:45 +0530

    fixed some css changes for local searcg and tags

--- components/shared/search/LocalSearch.tsx
+++ components/shared/search/LocalSearch.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const LocalSearch = ({ placeholder }: Props) => {
   return (
-    <div className="background-light800_darkgradient light-border-2 flex w-full gap-4 rounded-lg border px-4 py-2 dark:border-none">
+    <div className="background-light800_darkgradient light-border flex w-full gap-4 rounded-lg border px-4 py-2">
       <Image
         src="/assets/icons/search.svg"
         alt="local-search"
