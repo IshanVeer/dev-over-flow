b7a0091f88d79edba2e36e66264a8a1470f0f4fe
Ishan Veer <ishanpveer@gmail.com>
2025-03-07 16:07:48 +0530

    added separate component for stat badges and changed ctyling

--- components/shared/Stats.tsx
+++ components/shared/Stats.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 interface UserInfo {
@@ -7,12 +8,29 @@ interface UserInfo {
 interface Props {
   userInfo: UserInfo;
 }
+interface StatProps {
+  imageUrl: string;
+  imageAlt: string;
+  value: number;
+  title: string;
+}
+const BadgeStatCard = ({ imageUrl, imageAlt, value, title }: StatProps) => {
+  return (
+    <div className="background-light900_dark300 text-dark200_light900  flex items-center justify-between gap-8 rounded-lg px-10 py-7 shadow-sm max-xl:flex-col max-[880px]:flex-row ">
+      <Image src={imageUrl} width={30} height={30} alt={imageAlt} />
+      <div className="flex flex-col items-center">
+        <p>{value}</p>
+        <p>{title}</p>
+      </div>
+    </div>
+  );
+};
 
 const Stats = ({ userInfo }: Props) => {
   return (
     <div className="light-border  flex items-center justify-between gap-4 max-[880px]:grid max-[880px]:grid-cols-2  max-xs:grid-cols-1 ">
       {/* Questions / Answers */}
-      <div className="flex  items-center justify-between gap-8 rounded-lg bg-white px-10 py-7 shadow-sm max-xl:flex-col max-[880px]:flex-row ">
+      <div className="background-light900_dark300 text-dark200_light900  flex items-center justify-between gap-8 rounded-lg px-10 py-7 shadow-sm max-xl:flex-col max-[880px]:flex-row ">
         <div className="flex flex-col items-center">
           <p className="body-semibold">{userInfo.totalQuestions}</p>{" "}
           <p>Questions</p>
@@ -23,38 +41,29 @@ const Stats = ({ userInfo }: Props) => {
         </div>
       </div>
       {/* Gold Badges */}
-      <div className="flex  items-center justify-between gap-8 rounded-lg bg-white px-10 py-7 shadow-sm max-xl:flex-col max-[880px]:flex-row ">
-        <div className="flex flex-col items-center">
-          <p className="body-semibold">{userInfo.totalQuestions}</p>{" "}
-          <p>Questions</p>
-        </div>
-        <div className="flex flex-col items-center">
-          <p className="body-semibold">{userInfo.totalAnswers}</p>{" "}
-          <p>Answers</p>
-        </div>
-      </div>
+
+      <BadgeStatCard
+        imageUrl="/assets/icons/gold-medal.svg"
+        imageAlt="gold-badge"
+        value={0}
+        title="gold badges"
+      />
+
       {/* Silver Badges */}
-      <div className="flex  items-center justify-between gap-8 rounded-lg bg-white px-10 py-7 shadow-sm max-xl:flex-col max-[880px]:flex-row ">
-        <div className="flex flex-col items-center">
-          <p className="body-semibold">{userInfo.totalQuestions}</p>{" "}
-          <p>Questions</p>
-        </div>
-        <div className="flex flex-col items-center">
-          <p className="body-semibold">{userInfo.totalAnswers}</p>{" "}
-          <p>Answers</p>
-        </div>
-      </div>
+
+      <BadgeStatCard
+        imageUrl="/assets/icons/silver-medal.svg"
+        imageAlt="silver-badge"
+        value={0}
+        title="silver badges"
+      />
       {/* Bronze Badges */}
-      <div className="flex  items-center justify-between gap-8 rounded-lg bg-white px-10 py-7 shadow-sm max-xl:flex-col max-[880px]:flex-row ">
-        <div className="flex flex-col items-center">
-          <p className="body-semibold">{userInfo.totalQuestions}</p>{" "}
-          <p>Questions</p>
-        </div>
-        <div className="flex flex-col items-center">
-          <p className="body-semibold">{userInfo.totalAnswers}</p>{" "}
-          <p>Answers</p>
-        </div>
-      </div>
+      <BadgeStatCard
+        imageUrl="/assets/icons/bronze-medal.svg"
+        imageAlt="bronze-badge"
+        value={0}
+        title="bronze badges"
+      />
     </div>
   );
 };
