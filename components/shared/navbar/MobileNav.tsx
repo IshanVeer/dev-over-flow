ffe864ef8bfedef625e145b66b01f64446f6e608
Ishan Veer <ishanpveer@gmail.com>
2025-09-20 21:43:13 +0530

    created mobile hamburger sheet navigation

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -3,17 +3,14 @@ import { useTheme } from "@/context/ThemeProvider";
 import Image from "next/image";
 import React from "react";
 import Theme from "../Theme";
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { sidebarLinks } from "@/constants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const MobileNav = () => {
   const { mode } = useTheme();
+  const pathname = usePathname();
   return (
     <div className="min-sm:hidden py-4 px-5 flex items-center justify-between background-light900_dark300">
       {mode === "dark" ? (
@@ -44,14 +41,48 @@ const MobileNav = () => {
               className="invert-colors"
             />
           </SheetTrigger>
-          <SheetContent side="left">
-            <SheetHeader>
-              <SheetTitle>Are you absolutely sure?</SheetTitle>
-              <SheetDescription>
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
-              </SheetDescription>
-            </SheetHeader>
+          <SheetContent side="left" className="py-4 px-5">
+            {mode === "dark" ? (
+              <Image
+                src="/assets/images/logo-dark.svg"
+                alt="logo-light"
+                height={120}
+                width={120}
+                className="text-primary-500"
+              />
+            ) : (
+              <Image
+                src="/assets/images/logo-light.svg"
+                alt="logo-light"
+                height={120}
+                width={120}
+              />
+            )}
+            <ul className="flex flex-col gap-6 mt-16">
+              {sidebarLinks.map((link) => (
+                <li
+                  key={link.route}
+                  className={`${
+                    pathname === link.route
+                      ? "primary-gradient text-light-900"
+                      : ""
+                  } p-4 rounded-[8px]`}
+                >
+                  <Link href={link.route} className="flex items-center gap-4">
+                    <Image
+                      src={link.imgURL}
+                      alt={link.label}
+                      height={20}
+                      width={20}
+                      className={`${
+                        pathname === link.route ? "" : "invert-colors"
+                      }`}
+                    />
+                    <p className="base-semibold">{link.label}</p>
+                  </Link>
+                </li>
+              ))}
+            </ul>
           </SheetContent>
         </Sheet>
       </div>
