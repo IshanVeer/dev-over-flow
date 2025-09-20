b1d87bb75dbcc04d8bde12520bf179ec1ee89a33
Ishan Veer <ishanpveer@gmail.com>
2025-09-20 20:52:41 +0530

    user can switch between light and dark mode

--- components/shared/Theme.tsx
+++ components/shared/Theme.tsx
@@ -14,33 +14,55 @@ const Theme = () => {
   const { mode, setMode } = useTheme();
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger>
+      <DropdownMenuTrigger className="outline-0 focus:outline-0 cursor-pointer">
         {mode === "dark" ? (
           <Image
             src="/assets/icons/moon.svg"
             alt="dark"
-            height={20}
-            width={20}
+            height={24}
+            width={24}
             className="active-theme"
           />
         ) : (
           <Image
             src="/assets/icons/sun.svg"
             alt="dark"
-            height={20}
-            width={20}
+            height={24}
+            width={24}
             className="active-theme"
           />
         )}
       </DropdownMenuTrigger>
-      <DropdownMenuContent>
+      <DropdownMenuContent className="flex flex-col gap-2 py-2">
         {themes.map((theme) => (
           <DropdownMenuItem
-            className="flex items-center gap-2"
+            className="flex items-center gap-3"
             key={theme.value}
+            onClick={() => {
+              setMode(theme.value);
+              if (localStorage.theme !== "system") {
+                localStorage.theme = theme.value;
+              } else {
+                localStorage.removeItem("theme");
+              }
+            }}
           >
-            <Image src={theme.icon} alt={theme.label} height={20} width={20} />
-            {theme.label}
+            <Image
+              className={mode === theme.value ? "active-theme" : ""}
+              src={theme.icon}
+              alt={theme.label}
+              height={20}
+              width={20}
+            />
+            <p
+              className={`${
+                mode === theme.value
+                  ? "text-primary-500"
+                  : "text-dark100_light900"
+              } body-semibold  `}
+            >
+              {theme.label}
+            </p>
           </DropdownMenuItem>
         ))}
       </DropdownMenuContent>
