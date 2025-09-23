c2c3275225034d7bc1b0166ca22f486eb65d29fa
Ishan Veer <ishanpveer@gmail.com>
2025-09-23 12:42:11 +0530

    created auth layouts and few changes in the button

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,4 +1,5 @@
-import Button from "@/components/ui/Button";
+import CustomButton from "@/components/ui/CustomButton";
+
 import connectToDatabase from "@/lib/mongoose";
 
 export default function Home() {
@@ -8,7 +9,7 @@ export default function Home() {
       {/* title */}
       <div className="flex items-center justify-between">
         <h2 className="h1-bold text-dark100_light900">All Questions</h2>
-        <Button isLink label="Ask Question" route="/" />
+        <CustomButton isLink label="Ask Question" route="/" />
       </div>
 
       {/* local search */}
--- components/ui/CustomButton.tsx
+++ components/ui/CustomButton.tsx
@@ -7,7 +7,7 @@ interface ButtonProps {
   route?: string;
 }
 
-const Button = ({ isLink, label, route }: ButtonProps) => {
+const CustomButton = ({ isLink, label, route }: ButtonProps) => {
   const buttonClasses =
     "paragraph-semibold primary-gradient text-light-900 px-4 py-3 rounded-[8px] hover:primary-gradient-dark transition duration-150";
 
@@ -24,4 +24,4 @@ const Button = ({ isLink, label, route }: ButtonProps) => {
   );
 };
 
-export default Button;
+export default CustomButton;
